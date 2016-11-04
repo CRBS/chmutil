12780d5364d15d7a753b38c5844f4752d2d4048b
Chris Churas <churas@gmail.com>
2016-11-04 13:25:33 -0700

    finished unit test implementation

--- tests/test_batchedjoblistgenerator.py
+++ tests/test_batchedjoblistgenerator.py
@@ -77,3 +77,132 @@ class TestBatchedJobsListGenerator(unittest.TestCase):
         finally:
             shutil.rmtree(temp_dir)
 
+    def test_write_batched_job_config(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            noimg = os.path.join(temp_dir, 'image1.png')
+            img = os.path.join(temp_dir, 'image2.png')
+            open(img, 'a').close()
+
+            config = configparser.ConfigParser()
+            config.add_section('1')
+            config.set('1', CHMJobCreator.CONFIG_OUTPUT_IMAGE, img)
+            config.add_section('2')
+            config.set('2', CHMJobCreator.CONFIG_OUTPUT_IMAGE, noimg)
+
+            chmconfig = CHMConfig('images', 'model', temp_dir,
+                                  '500x500', '20x20',
+                                  config=config)
+
+            gen = BatchedJobsListGenerator(chmconfig)
+            bconfig = configparser.ConfigParser()
+            bconfig.add_section('1')
+            bconfig.set('', CHMJobCreator.BCONFIG_TASK_ID,
+                        ' 1,2,3')
+            # try writing where previous file does NOT exist
+            gen._write_batched_job_config(bconfig)
+            self.assertTrue(os.path.isfile(chmconfig.get_batchedjob_config()))
+            self.assertFalse(os.path.isfile(chmconfig.get_batchedjob_config() +
+                                            BatchedJobsListGenerator.OLD_SUFFIX))
+
+            # try writing where there is a previous file
+            gen._write_batched_job_config(bconfig)
+            self.assertTrue(os.path.isfile(chmconfig.get_batchedjob_config()))
+            self.assertTrue(os.path.isfile(chmconfig.get_batchedjob_config() +
+                                            BatchedJobsListGenerator.OLD_SUFFIX))
+
+        finally:
+            shutil.rmtree(temp_dir)
+
+
+    def test_generate_batched_jobs_list_all_jobs_complete(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            img = os.path.join(temp_dir, 'image2.png')
+            open(img, 'a').close()
+
+            config = configparser.ConfigParser()
+            config.add_section('1')
+            config.set('1', CHMJobCreator.CONFIG_OUTPUT_IMAGE, img)
+            chmconfig = CHMConfig('images', 'model', temp_dir,
+                                  '500x500', '20x20',
+                                  config=config)
+
+            gen = BatchedJobsListGenerator(chmconfig)
+            self.assertEqual(gen.generate_batched_jobs_list(), 0)
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_generate_batched_jobs_list_one_job_not_complete(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            img = os.path.join(temp_dir, 'image2.png')
+
+            config = configparser.ConfigParser()
+            config.add_section('1')
+            config.set('1', CHMJobCreator.CONFIG_OUTPUT_IMAGE, img)
+            chmconfig = CHMConfig('images', 'model', temp_dir,
+                                  '500x500', '20x20',
+                                  config=config)
+
+            gen = BatchedJobsListGenerator(chmconfig)
+            self.assertEqual(gen.generate_batched_jobs_list(), 1)
+            bconfig = configparser.ConfigParser()
+            bconfig.read(chmconfig.get_batchedjob_config())
+            self.assertEqual(bconfig.get('1', CHMJobCreator.BCONFIG_TASK_ID),
+                             '1')
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_generate_batched_jobs_list_two_jobs_not_complete(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            img = os.path.join(temp_dir, 'image2.png')
+            img2 = os.path.join(temp_dir, 'image3.png')
+
+            config = configparser.ConfigParser()
+            config.add_section('1')
+            config.set('1', CHMJobCreator.CONFIG_OUTPUT_IMAGE, img)
+            config.add_section('2')
+            config.set('2', CHMJobCreator.CONFIG_OUTPUT_IMAGE, img2)
+
+            chmconfig = CHMConfig('images', 'model', temp_dir,
+                                  '500x500', '20x20',
+                                  config=config)
+
+            gen = BatchedJobsListGenerator(chmconfig)
+            self.assertEqual(gen.generate_batched_jobs_list(), 2)
+            bconfig = configparser.ConfigParser()
+            bconfig.read(chmconfig.get_batchedjob_config())
+            self.assertEqual(bconfig.get('1', CHMJobCreator.BCONFIG_TASK_ID),
+                             '1')
+            self.assertEqual(bconfig.get('2', CHMJobCreator.BCONFIG_TASK_ID),
+                             '2')
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_generate_batched_jobs_list_two_jobs_batched_not_complete(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            img = os.path.join(temp_dir, 'image2.png')
+            img2 = os.path.join(temp_dir, 'image3.png')
+
+            config = configparser.ConfigParser()
+            config.add_section('1')
+            config.set('1', CHMJobCreator.CONFIG_OUTPUT_IMAGE, img)
+            config.add_section('2')
+            config.set('2', CHMJobCreator.CONFIG_OUTPUT_IMAGE, img2)
+
+            chmconfig = CHMConfig('images', 'model', temp_dir,
+                                  '500x500', '20x20',
+                                  jobs_per_node=2,
+                                  config=config)
+
+            gen = BatchedJobsListGenerator(chmconfig)
+            self.assertEqual(gen.generate_batched_jobs_list(), 1)
+            bconfig = configparser.ConfigParser()
+            bconfig.read(chmconfig.get_batchedjob_config())
+            self.assertEqual(bconfig.get('1', CHMJobCreator.BCONFIG_TASK_ID),
+                             '1,2')
+        finally:
+            shutil.rmtree(temp_dir)
