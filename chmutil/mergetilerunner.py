ab094088d7fed422fa54345d77f3395289995a27
Chris Churas <churas@gmail.com>
2016-11-14 15:48:35 -0800

    initial revision

---
+++ chmutil/mergetilerunner.py
@@ -0,0 +1,208 @@
+#! /usr/bin/env python
+
+import sys
+import os
+import argparse
+import logging
+import uuid
+import configparser
+import subprocess
+import shlex
+import shutil
+import chmutil
+import time
+
+from chmutil.core import CHMJobCreator
+from chmutil.core import CHMConfigFromConfigFactory
+from chmutil.core import Parameters
+from chmutil import core
+
+LOG_FORMAT = "%(asctime)-15s %(levelname)s (%(process)d) %(name)s %(message)s"
+
+# create logger
+logger = logging.getLogger('chmutil.chmrunner')
+
+
+def _parse_arguments(desc, args):
+    """Parses command line arguments using argparse.
+    """
+    parsed_arguments = Parameters()
+
+    help_formatter = argparse.RawDescriptionHelpFormatter
+    parser = argparse.ArgumentParser(description=desc,
+                                     formatter_class=help_formatter)
+    parser.add_argument("taskid", help='Task id')
+    parser.add_argument("jobdir", help='Directory containing chm.list.job'
+                                       'file')
+    parser.add_argument("--scratchdir", help='Scratch Directory '
+                                             '(default /tmp)',
+                        default='/tmp')
+    parser.add_argument("--log", dest="loglevel", choices=['DEBUG',
+                        'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+                        help="Set the logging level (default WARNING)",
+                        default='WARNING')
+    parser.add_argument('--version', action='version',
+                        version=('%(prog)s ' + chmutil.__version__))
+
+    return parser.parse_args(args, namespace=parsed_arguments)
+
+
+def _run_external_command(cmd_to_run, tmp_dir):
+    """Runs command
+    """
+    # TODO MOVE THIS INTO A SEPARATE RUN CLASS
+    if cmd_to_run is None:
+        return 256, '', 'Command must be set'
+
+    logger.info("Running command " + cmd_to_run)
+    tmp_stdout = os.path.join(tmp_dir, 'stdout.txt')
+    stdout_f = open(tmp_stdout, 'w')
+    tmp_stderr = os.path.join(tmp_dir, 'stderr.txt')
+    stderr_f = open(tmp_stderr, 'w')
+    p = subprocess.Popen(shlex.split(cmd_to_run),
+                         stdout=stdout_f,
+                         stderr=stderr_f)
+
+    logger.debug('Waiting for process to complete')
+    p.poll()
+
+    while p.returncode is None:
+        time.sleep(10)
+        p.poll()
+
+    stdout_f.flush()
+    stdout_f.close()
+    stderr_f.flush()
+    stderr_f.close()
+
+    fo = open(tmp_stdout, 'r')
+    out = fo.read()
+    fo.close()
+    fe = open(tmp_stderr, 'r')
+    err = fe.read()
+    fe.close()
+
+    return p.returncode, out, err
+
+
+def _run_merge_job(theargs):
+    """Runs all jobs for task
+    """
+    cfac = CHMConfigFromConfigFactory(theargs.jobdir)
+    chmconfig = cfac.get_chmconfig(skip_loading_config=True,
+                                   skip_loading_mergeconfig=False)
+    if not os.path.isfile(chmconfig.get_batched_mergejob_config()):
+        logger.error('No batched merge config found: ' +
+                     chmconfig.get_batched_mergejob_config())
+        return 1
+    return _run_jobs(chmconfig, theargs, theargs.taskid)
+
+
+def _run_jobs(chmconfig, theargs, taskid):
+    """Runs jobs for task in parallel
+    """
+    bconfig = configparser.ConfigParser()
+    bconfig.read(chmconfig.get_batched_mergejob_config())
+    tasks = bconfig.get(taskid, CHMJobCreator.BCONFIG_TASK_ID).split(',')
+    process_list = []
+    logger.debug('Running ' + str(len(tasks)) + 'child processes')
+    for t in tasks:
+        pid = os.fork()
+        if pid is 0:
+            logger.debug('In child submitting job to run task ' + t)
+            return _run_single_merge_job(theargs, t)
+        else:
+            logger.debug('Appending child process to list: ' + str(pid))
+            process_list.append(pid)
+
+    exit_code = 0
+    running_procs = len(process_list)
+    while running_procs > 0:
+        logger.info('Still waiting on ' + str(running_procs) + ' processes')
+        logger.debug('Waiting on pid: ' + str(process_list[0]))
+        try:
+            ecode = os.waitpid(process_list[0], 0)[1]
+            if ecode > 255:
+                ecode = ecode >> 8
+            logger.info('Process ' + str(process_list[0]) +
+                        ' exited with code: ' + str(ecode))
+            exit_code += ecode
+        except OSError:
+            logger.exception('Caught exception, but it might not '
+                             'be a big deal')
+        time.sleep(1)
+        del process_list[0]
+        running_procs = len(process_list)
+    return exit_code
+
+
+def _run_single_merge_job(theargs, taskid):
+    """runs CHM Job
+    :param theargs: list of arguments obtained from _parse_arguments()
+    :returns: exit code for program. 0 success otherwise failure
+    """
+    # TODO REFACTOR THIS INTO FACTORY CLASS TO GET CONFIG
+    # TODO REFACTOR THIS INTO CLASS TO GENERATE CHM JOB COMMAND
+    out_dir = None
+    try:
+        out_dir = os.path.join(theargs.scratchdir, uuid.uuid4().hex)
+        config = configparser.ConfigParser()
+        config.read(os.path.join(theargs.jobdir,
+                    CHMJobCreator.CONFIG_FILE_NAME))
+        input_dir = config.get(taskid,
+                               CHMJobCreator.MERGE_INPUT_IMAGE_DIR)
+        out_file = config.get(taskid,
+                              CHMJobCreator.MERGE_OUTPUT_IMAGE)
+        logger.debug('Creating directory ' + out_dir)
+        os.makedirs(out_dir, mode=0o775)
+        cmd = (config.get(CHMJobCreator.CONFIG_DEFAULT,
+                          CHMJobCreator.MERGE_MERGETILES_BIN) + ' ' +
+               input_dir + ' ' + out_file + ' --suffix png ')
+        exitcode, out, err = _run_external_command(cmd, out_dir)
+
+        sys.stdout.write(out)
+        sys.stderr.write(err)
+        sys.stdout.flush()
+        sys.stderr.flush()
+        return exitcode
+    except Exception:
+        logger.exception("Error caught exception")
+        return 2
+    finally:
+        if out_dir is not None:
+            if os.path.isdir(out_dir):
+                shutil.rmtree(out_dir)
+
+
+def main(arglist):
+    """Main function
+    :param arglist: Should be set to sys.argv which is list of arguments
+                    passed on commandline including script being run as arg 0
+    :returns: exit code. 0 is success otherwise failure
+    """
+    desc = """
+              Version {version}
+
+              Runs Merge tiles for <taskid> specified on command
+              line.
+
+
+              Example Usage:
+
+              mergetilerunner.py 1 /foo/chmjob --scratchdir /scratch
+
+              """.format(version=chmutil.__version__)
+
+    theargs = _parse_arguments(desc, arglist[1:])
+    theargs.program = arglist[0]
+    theargs.version = chmutil.__version__
+    core.setup_logging(logger, log_format=LOG_FORMAT,
+                       loglevel=theargs.loglevel)
+    try:
+        return _run_merge_job(theargs)
+    finally:
+        logging.shutdown()
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
