6abbf5e4f8510c445c77e292d7cd5f5708d23c6e
Chris Churas <churas@gmail.com>
2016-11-30 15:15:00 -0800

    added ClusterFactory and helper methods to RocceCluster

--- chmutil/cluster.py
+++ chmutil/cluster.py
@@ -121,16 +121,48 @@ class BatchedJobsListGenerator(object):
 class RocceCluster(object):
     """Generates submit script for CHM job on Rocce cluster
     """
-    SUBMIT_SCRIPT_NAME = 'runjobs.rocce'
-    MERGE_SUBMIT_SCRIPT_NAME = 'runmerge.rocce'
+    CLUSTER = 'rocce'
+    SUBMIT_SCRIPT_NAME = 'runjobs.' + CLUSTER
+    MERGE_SUBMIT_SCRIPT_NAME = 'runmerge' + CLUSTER
     CHMRUNNER = 'chmrunner.py'
     MERGERUNNER = 'mergetilerunner.py'
+    RUNCHMJOB = 'runchmjob.py'
+    DEFAULT_JOBS_PER_NODE = 1
 
     def __init__(self, chmconfig):
         """Constructor
         :param chmconfig: CHMConfig object for the job
         """
         self._chmconfig = chmconfig
+
+    def set_chmconfig(self, chmconfig):
+        """Sets CHMConfig object
+        :param chmconfig: CHMConfig object
+        """
+        self._chmconfig = chmconfig
+
+    def get_suggested_jobs_per_node(self, jobs_per_node):
+        """Returns suggested jobs per node for cluster
+        :returns: 1 as int
+        """
+        if jobs_per_node is None:
+            logger.debug('Using default since jobs per node is None')
+            return RocceCluster.DEFAULT_JOBS_PER_NODE
+
+        if jobs_per_node <= 0:
+            logger.debug('Using default since jobs per node is 0 or less')
+            return RocceCluster.DEFAULT_JOBS_PER_NODE
+        try:
+            return int(jobs_per_node)
+        except ValueError:
+            logger.debug('Using default since jobs per int conversion failed')
+            return RocceCluster.DEFAULT_JOBS_PER_NODE
+
+    def get_cluster(self):
+        """Returns cluster name which is rocce
+        :returns: name of cluster as string in this case rocce
+        """
+        return RocceCluster.CLUSTER
 
     def _get_submit_script_path(self):
         """Gets path to submit script
@@ -237,6 +269,16 @@ class RocceCluster(object):
         os.chmod(script, stat.S_IRWXU | stat.S_IRGRP | stat.S_IROTH)
         return script
 
+    def get_runchmjob_submit_command(self):
+        """Returns runchmjob.py command the user should run
+        :returns: string containing runchmjob.py the user should invoke
+        """
+        runchm =  os.path.join(self._chmconfig.get_script_bin(),
+                               RocceCluster.RUNCHMJOB)
+        val = (runchm + ' "' + self._chmconfig.get_out_dir() +
+               '" --cluster ' + self.get_cluster())
+        return val
+
     def get_chm_submit_command(self, number_jobs):
         """Returns submit command user should invoke
            to run jobs on scheduler
@@ -254,3 +296,33 @@ class RocceCluster(object):
                'qsub -t 1-' + str(number_jobs) + ' ' +
                RocceCluster.MERGE_SUBMIT_SCRIPT_NAME)
         return val
+
+
+class ClusterFactory(object):
+    """Factory that produces cluster objects based on cluster name
+    """
+    # WARNING
+    VALID_CLUSTERS = [RocceCluster.CLUSTER]
+
+    def __init__(self):
+        """Constructor
+        """
+        pass
+
+    def get_cluster_by_name(self, name):
+        """Creates Cluster object based on value of `name` passed in
+        :param name: string containing name of cluster
+        :returns: Cluster object for that cluster or None if no match
+        """
+        if name is None:
+            logger.error('name passed in is None')
+            return None
+
+        lc_cluster = name.lower()
+
+        if lc_cluster == RocceCluster.CLUSTER:
+            logger.debug('returning RocceCluster')
+            return RocceCluster(None)
+
+        logger.error('No cluster class supporting ' + lc_cluster + ' found')
+        return None
