c8c96ac21b744da42e920b95c0f5828679672dbc
Chris Churas <churas@gmail.com>
2017-06-21 13:50:41 -0700

    moved ImageStatsFromDirectoryFactory, ImageStats, get_image_path_list, and InvalidImageDirError into image.py and added implementation for ImageStatsSummary

--- chmutil/image.py
+++ chmutil/image.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import os
 import math
 import logging
 from PIL import Image
@@ -13,6 +14,51 @@ class InvalidImageError(Exception):
     """Denotes invalid image object
     """
     pass
+
+
+class InvalidImageDirError(Exception):
+    """Raised when invalid image directory is used
+    """
+    pass
+
+
+def get_image_path_list(image_dir, suffix,
+                        keysortfunc=None):
+    """Gets list of images with suffix from dir
+    :param image_dir: Path to directory with images
+    :param suffix: Only include files ending with suffix.
+                   code uses str().endswidth for checking.
+                   If `suffix` is None then all files match
+    :raises InvalidImageDirError: if `image_dir` is None or not
+                                  a directory
+    :raises OSError: if there is an error invoking os.listdir on `image_dir`
+    :returns: list of file paths
+    """
+    if image_dir is None:
+        raise InvalidImageDirError('image_dir is None')
+
+    if not os.path.isdir(image_dir):
+        raise InvalidImageDirError('image_dir must be a directory')
+
+    if suffix is None:
+        adjusted_suffix = ''
+    else:
+        adjusted_suffix = suffix
+
+    img_list = []
+    for entry in os.listdir(image_dir):
+        fp = os.path.join(image_dir, entry)
+        if not os.path.isfile(fp):
+            logger.debug(entry + ' is not a file. skipping')
+            continue
+        if entry.endswith(adjusted_suffix):
+            img_list.append(fp)
+
+    if keysortfunc is not None:
+        logger.debug('Sort function passed in sorting data')
+        img_list.sort(key=keysortfunc)
+
+    return img_list
 
 
 class SimpleImageMerger(object):
@@ -267,3 +313,141 @@ class SingleColumnImageTileGenerator(object):
             box = (0, offset, width, cur_tile_height)
             logger.debug('Returning tile = ' + str(box))
             yield ImageTile(image.crop(box), box=box)
+
+
+class ImageStats(object):
+    """Contains information about an image to be segmented
+    """
+    def __init__(self, path, width, height, format,
+                 size_in_bytes=0):
+        """Constructor
+        """
+        self._path = path
+        self._width = width
+        self._height = height
+        self._format = format
+        self._size_in_bytes = size_in_bytes
+
+    def get_width(self):
+        """Gets width
+        """
+        return self._width
+
+    def get_height(self):
+        """Gets height
+        """
+        return self._height
+
+    def get_file_path(self):
+        """Gets path to image
+        """
+        return self._path
+
+    def get_format(self):
+        """
+        :return:
+        """
+        return self._format
+
+    def get_size_in_bytes(self):
+        """ Gets size in bytes
+        :return: size in bytes
+        """
+        return self._size_in_bytes
+
+
+class ImageStatsFromDirectoryFactory(object):
+    """Creates ImageStats objects from directory of images
+    """
+
+    def __init__(self, directory, max_image_pixels=768000000):
+        """Constructor
+        """
+        self._directory = directory
+        logger.debug('Setting MAX_IMAGE_PIXELS to ' + str(max_image_pixels))
+        Image.MAX_IMAGE_PIXELS = max_image_pixels
+
+    def get_input_image_stats(self,
+                              keysortfunc=None):
+        """Gets InputImageStats objects as list
+        """
+        image_stats_list = []
+        if os.path.isfile(self._directory):
+            return []
+        file_list = get_image_path_list(self._directory,
+                                        None)
+        for fp in file_list:
+            im = None
+            try:
+                im = Image.open(fp)
+                iis = ImageStats(fp, im.size[0],
+                                 im.size[1], im.format,
+                                 size_in_bytes=os.path.getsize(fp))
+                image_stats_list.append(iis)
+            except Exception:
+                logger.exception('Skipping file unable to open ' + fp)
+            finally:
+                try:
+                    im.close()
+                except Exception:
+                    logger.exception('Caught exception attempting '
+                                     'to close image')
+
+        return image_stats_list
+
+
+class ImageStatsSummary(object):
+    """Represents summary information for multiple ImageStats
+       objects
+    """
+    def __init__(self):
+        """Constructor
+        """
+        self._num_images = 0
+        self._total_size = 0
+        self._total_pixels = 0
+        self._dimension_hash = {}
+
+    def add_image_stats(self, image_stats):
+        """Adds `ImageStats` (image_stats) to this summary
+        :param image_stats: ImageStats object
+        """
+        if image_stats is None:
+            logger.warning('None passed into add_image_stats')
+            return
+
+        self._num_images += 1
+        self._total_size += image_stats.get_size_in_bytes()
+        dim_tuple = (image_stats.get_width(), image_stats.get_height())
+        if dim_tuple in self._dimension_hash.keys():
+            self._dimension_hash[dim_tuple] += 1
+        else:
+            self._dimension_hash[dim_tuple] = 1
+        self._total_pixels += dim_tuple[0] * dim_tuple[1]
+
+    def get_image_count(self):
+        """Returns number of images
+        :returns: total number of images
+        """
+        return self._num_images
+
+    def get_total_size_of_images_in_bytes(self):
+        """Returns total size of images in bytes
+        :returns: total size of all images in bytes
+        """
+        return self._total_size
+
+    def get_image_dimensions_as_dict(self):
+        """Gets dictionary of image dimensions.
+        :returns: dictionary of image dimensions where key is
+                  tuple (W,H) and value is number of images with
+                  given image dimension
+        """
+        return self._dimension_hash
+
+    def get_total_pixels(self):
+        """Gets total number of pixels in images
+        :returns: total number of pixels ie W x H x # images
+        """
+        return self._total_pixels
+
